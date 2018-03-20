17c08ccb439cf8699d8c012d12c083b49538ea22
T33441 <kucukbagci.yilmaz@outlook.com>
2018-03-20 17:48:58 +0300

    fsdafs

--- ykbDeneme/Startup.cs
+++ ykbDeneme/Startup.cs
@@ -27,7 +27,7 @@ namespace ykbDeneme
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello fsdfWorld!");
             });
         }
     }
